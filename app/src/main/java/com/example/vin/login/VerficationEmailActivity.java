604453025e6a8951e82fc3c0043405ad568add7c
Vindener <vindener12@gmail.com>
2023-05-16 16:53:19 +0300

    Added Login and Verication Activity

--- app/src/main/java/com/example/vin/login/VerficationEmailActivity.java
+++ app/src/main/java/com/example/vin/login/VerficationEmailActivity.java
@@ -1,38 +1,37 @@
 package com.example.vin.login;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
-import com.example.vin.LoadActivity;
+import com.example.vin.MainActivity;
 import com.example.vin.R;
 
-public class LoginActivity extends AppCompatActivity {
-    private Button bth_login;
-    private String email;
+public class VerficationEmailActivity extends AppCompatActivity {
+    private Button bth_verfication_email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_verfication_email);
 
-        email = findViewById(R.id.TextEmailAddress).toString().trim();
+        bth_verfication_email = findViewById(R.id.bth_verfication_email);
 
-        bth_login = findViewById(R.id.bth_login);
-        bth_login.setOnClickListener(new View.OnClickListener() {
+        bth_verfication_email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(LoginActivity.this, "Result: ", Toast.LENGTH_SHORT).show();
 
-                Intent myIntent = new Intent(LoginActivity.this, VerficationEmailActivity.class);
-                LoginActivity.this.startActivity(myIntent);
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isFirstRun", false);
+                editor.apply();
+
+                Intent myIntent = new Intent(VerficationEmailActivity.this, MainActivity.class);
+                VerficationEmailActivity.this.startActivity(myIntent);
                 finish();
 //                SharedPreferences login = context.getSharedPreferences("MyPrefs", MODE_PRIVATE);
 //                SharedPreferences.Editor edit = login.edit();
@@ -43,7 +42,6 @@ public class LoginActivity extends AppCompatActivity {
                 // Вы можете выполнить здесь необходимые действия при нажатии кнопки
             }
         });
-
 
 
     }

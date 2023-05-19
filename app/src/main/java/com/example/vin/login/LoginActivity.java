014d2ba9713b21a5506c94441d849d2d463b246a
Vindener <vindener12@gmail.com>
2023-05-19 10:29:25 +0300

    Added Server Connection and Create User

--- app/src/main/java/com/example/vin/login/LoginActivity.java
+++ app/src/main/java/com/example/vin/login/LoginActivity.java
@@ -17,6 +17,15 @@ import android.widget.Toast;
 import com.example.vin.LoadActivity;
 import com.example.vin.R;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.example.vin.addition.CreateUser;
+
 public class LoginActivity extends AppCompatActivity {
     private Button bth_login;
     private String email;
@@ -38,6 +47,11 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else{
                     emailValidator(email);
+
+                    //ЗАПРОС ДО СЕРВЕР
+//                    CreateUser task = new CreateUser();
+//                    task.execute("testuser3","0955598","l23h");
+
                 }
 //                SharedPreferences login = context.getSharedPreferences("MyPrefs", MODE_PRIVATE);
 //                SharedPreferences.Editor edit = login.edit();
@@ -48,6 +62,50 @@ public class LoginActivity extends AppCompatActivity {
                 // Вы можете выполнить здесь необходимые действия при нажатии кнопки
             }
         });
+    }
+
+    public void ConnectToServer(){
+        try {
+            URL url = new URL("@string/ip_server"+"/createUser");
+
+            // Открытие соединения
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Установка метода запроса
+            connection.setRequestMethod("POST");
+
+            // Разрешение вывода данных
+            connection.setDoOutput(true);
+
+            // Параметры запроса (если необходимо)
+            String parameters = "kod=2&email=testuser&phone=09598&name=loh&wallet_id=2";
+            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(parameters);
+            outputStream.flush();
+            outputStream.close();
+
+            // Получение ответа от сервера
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Вывод ответа
+            System.out.println("Response: " + response.toString());
+
+            // Закрытие соединения
+            connection.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void emailValidator( String emailToText){
